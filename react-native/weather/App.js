0ba18d90a35fd51c6ba311622efafef36ec2c5c9
babo <seirion@mensakorea.org>
2019-03-12 11:17:23 +0900

    add Forecast

--- react-native/weather/App.js
+++ react-native/weather/App.js
@@ -1,24 +1,37 @@
 import React from 'react';
 import { StyleSheet, Text, TextInput, View } from 'react-native';
+import Forecast from "./Forecast";
 
 export default class WeatherProject extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            zip: ""
+            zip: "",
+            forecast: null
         }
     }
 
     _handleTextChange = event => {
         this.setState({ zip: event.nativeEvent.text });
-    }
+    };
 
     render() {
+        let content = null;
+        if (this.state.forecast != null) {
+            content = (
+                <Forecast
+                    main={this.state.forecast.main}
+                    description={this.state.forecast.description}
+                    temp={this.state.forecast.temp}
+                />
+            )
+        }
         return (
             <View style={styles.container}>
                 <Text style={styles.welcome}>
                     You input {this.state.zip}
                 </Text>
+                {content}
                 <TextInput
                     style={styles.input}
                     onSubmitEditing={this._handleTextChange}
