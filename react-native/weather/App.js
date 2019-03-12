bcdc83ecd90d8bb7ab62bd572df0378c89ee907b
babo <seirion@mensakorea.org>
2019-03-12 18:17:07 +0900

    fit the background image to the parent

--- react-native/weather/App.js
+++ react-native/weather/App.js
@@ -65,7 +65,6 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         backgroundColor: '#666666',
-        alignItems: 'center',
         justifyContent: 'center',
     },
     backDrop: {
