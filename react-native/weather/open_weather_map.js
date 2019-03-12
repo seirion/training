e3ee351a55c321fc21fdaa66e85eb5be0091fc4d
babo <seirion@mensakorea.org>
2019-03-12 16:02:15 +0900

    add open_weather_map for query forecast

---
+++ react-native/weather/open_weather_map.js
@@ -0,0 +1,23 @@
+const WEATHER_API_KEY = "bbeb34ebf60ad50f7893e7440a1e2b0b";
+const API_STEM = "http://api.openweathermap.org/data/2.5/weather?";
+
+function zipUrl(zip) {
+    return `${API_STEM}q=${zip}&units=imperial&APPID=${WEATHER_API_KEY}`;
+}
+
+function fetchForecast(zip) {
+    return fetch(zipUrl(zip))
+        .then(response => response.json())
+        .then(responseJson => {
+            return {
+                main: responseJson.weather[0].main,
+                description: responseJson.weather[0].description,
+                temp: responseJson.weather[0].temp
+            }
+        })
+        .catch(error => {
+            console.log(error)
+        });
+}
+
+export default { featchForecast };
