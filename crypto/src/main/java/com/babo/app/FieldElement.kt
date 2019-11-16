8f12351f0a7ef81dfd83f3b7ebbf3bb2bf256f28
babo <seirion@mensakorea.org>
2019-11-17 01:44:44 +0900

    multiply and pow
    - test perma's little theorem

--- crypto/src/main/java/com/babo/app/FieldElement.kt
+++ crypto/src/main/java/com/babo/app/FieldElement.kt
@@ -24,4 +24,24 @@ data class FieldElement(var num: Int, val prime: Int) {
         require(prime == other.prime)
         return FieldElement((prime + num - other.num) % prime, prime)
     }
+
+    fun multiply(other: FieldElement) = apply {
+        require(prime == other.prime)
+        num = (num * other.num) % prime
+    }
+
+    operator fun times(other: FieldElement): FieldElement {
+        require(prime == other.prime)
+        return FieldElement((num * other.num) % prime, prime)
+    }
+
+    fun pow(exponent: Int) = apply {
+        num = this.pow(num, exponent)
+    }
+
+    private fun pow(base: Int, exponent: Int) : Int = when {
+        exponent == 0 -> 1
+        exponent % 2 == 0 -> pow(base, exponent / 2).let { it * it % prime }
+        else -> base * pow(base, exponent - 1) % prime
+    }
 }
--- crypto/src/test/java/com/babo/app/FieldElementTest.kt
+++ crypto/src/test/java/com/babo/app/FieldElementTest.kt
@@ -31,4 +31,31 @@ class FieldElementTest {
         assertEquals(c , a - b)
         assertEquals(c , a.subtract(b))
     }
+
+    @Test
+    fun multiply() {
+        val a = FieldElement(3, 13)
+        val b = FieldElement(12, 13)
+        val c = FieldElement(10, 13)
+        assertEquals(c , a * b)
+        assertEquals(c , a.multiply(b))
+    }
+
+    @Test
+    fun power() {
+        val a = FieldElement(3, 13)
+        val b = FieldElement(1, 13)
+        assertEquals(b , a.pow(3))
+    }
+
+    @Test
+    fun perma_little_theorem() {
+        val primes = arrayListOf(7, 11, 17, 31)
+        primes.forEach { prime ->
+            val one = FieldElement(1, prime)
+            (1 until prime).forEach {
+                assertEquals(one, FieldElement(it, prime).pow(prime - 1))
+            }
+        }
+    }
 }
