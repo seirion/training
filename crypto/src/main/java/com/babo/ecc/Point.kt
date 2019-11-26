236de6cf1f704542bc943934a69ca1a7eae769c6
babo <seirion@mensakorea.org>
2019-11-26 20:33:38 +0900

    set operator immutable

--- crypto/src/main/java/com/babo/ecc/Point.kt
+++ crypto/src/main/java/com/babo/ecc/Point.kt
@@ -11,27 +11,17 @@ data class Point(var x: Int?, var y: Int?, val a: Int, val b: Int) {
         }
     }
 
-    fun add(other: Point) = apply {
-        require(a == other.a && b == other.b) {
-            "Points $this $other are not on the same curve"
-        }
-        if (x == null) return other
-        if (other.x == null) return this
-        if (y == -1 * other.y!!) {
-            x = null
-            y = null
-        }
-    }
+    fun add(other: Point) = this + other
 
     operator fun plus(other: Point): Point {
         require(a == other.a && b == other.b) {
             "Points $this $other are not on the same curve"
         }
         return when {
-            x == null -> Point(other.x, other.y, other.a, other.b)
-            other.x == null -> Point(x, y, a, b)
+            x == null -> other.copy()
+            other.x == null -> this.copy()
             y == -1 * other.y!! -> Point(null, null, a, b)
-            else -> this
+            else -> this.copy()
         }
     }
 }
