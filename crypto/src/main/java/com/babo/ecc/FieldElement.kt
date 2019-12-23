b0745f9c7f19b98898a12b4e8e4aeffccfd342bc
babo <seirion@mensakorea.org>
2019-12-23 17:35:15 +0900

    change num to BigInteger

--- crypto/src/main/java/com/babo/ecc/FieldElement.kt
+++ crypto/src/main/java/com/babo/ecc/FieldElement.kt
@@ -1,13 +1,10 @@
 package com.babo.ecc
 
-data class FieldElement(var num: Int, val prime: Int) {
-    init {
-        require(num in 0 until prime)
-    }
+import java.math.BigInteger
 
-    fun add(other: FieldElement) = apply {
-        require(prime == other.prime)
-        num = (num + other.num) % prime
+data class FieldElement(var num: BigInteger, val prime: BigInteger) {
+    init {
+        require(BigInteger.ZERO <= num && num < prime)
     }
 
     operator fun plus(other: FieldElement): FieldElement {
@@ -15,19 +12,9 @@ data class FieldElement(var num: Int, val prime: Int) {
         return FieldElement((num + other.num) % prime, prime)
     }
 
-    fun subtract(other: FieldElement) = apply {
-        require(prime == other.prime)
-        num = (prime + num - other.num) % prime
-    }
-
     operator fun minus(other: FieldElement): FieldElement {
         require(prime == other.prime)
         return FieldElement((prime + num - other.num) % prime, prime)
-    }
-
-    fun multiply(other: FieldElement) = apply {
-        require(prime == other.prime)
-        num = (num * other.num) % prime
     }
 
     operator fun times(other: FieldElement): FieldElement {
@@ -35,28 +22,25 @@ data class FieldElement(var num: Int, val prime: Int) {
         return FieldElement((num * other.num) % prime, prime)
     }
 
-    fun pow(exponent: Int) = apply {
-        var n = exponent % (prime - 1)
-        if (n < 0) n += prime - 1
+    fun pow(exponent: BigInteger) = apply {
+        var n = exponent % prime.subtract(BigInteger.ONE)
+        if (n < BigInteger.ZERO) n += prime.subtract(BigInteger.ONE)
         num = this.pow(num, n)
     }
 
-    private fun pow(base: Int, exponent: Int) : Int = when {
-        exponent == 0 -> 1
-        exponent % 2 == 0 -> pow(base, exponent / 2).let { it * it % prime }
-        else -> base * pow(base, exponent - 1) % prime
-    }
-
-    fun divide(other: FieldElement) = apply {
-        require(prime == other.prime)
-        val inverse = pow(other.num, prime - 2)
-        num = (num * inverse) % prime
+    private fun pow(base: BigInteger, exponent: BigInteger) : BigInteger = when {
+        exponent == BigInteger.ZERO -> BigInteger.ONE
+        exponent.mod(TWO) == BigInteger.ZERO -> pow(base, exponent.divide(TWO)).let { it * it % prime }
+        else -> base * pow(base, exponent - BigInteger.ONE) % prime
     }
 
     operator fun div(other: FieldElement): FieldElement {
         require(prime == other.prime)
-        val inverse = pow(other.num, prime - 2)
+        val inverse = pow(other.num, prime - TWO)
         return FieldElement((num * inverse) % prime, prime)
     }
 
+    companion object {
+        private val TWO = BigInteger.valueOf(2L)
+    }
 }
--- crypto/src/test/java/com/babo/ecc/FieldElementTest.kt
+++ crypto/src/test/java/com/babo/ecc/FieldElementTest.kt
@@ -3,73 +3,75 @@ package com.babo.ecc
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Assertions.assertNotEquals
 import org.junit.jupiter.api.Test
+import java.math.BigInteger
 
 class FieldElementTest {
 
     @Test
     fun filedEquality() {
-        val a = FieldElement(7, 13)
-        val b = FieldElement(6, 13)
+        val a = FieldElement(BigInteger.valueOf(7), BigInteger.valueOf(13))
+        val b = FieldElement(BigInteger.valueOf(6), BigInteger.valueOf(13))
         assertNotEquals(a, b)
         assertEquals(a, a)
     }
 
     @Test
     fun addition() {
-        val a = FieldElement(7, 13)
-        val b = FieldElement(12, 13)
-        val c = FieldElement(6, 13)
+        val a = FieldElement(BigInteger.valueOf(7), BigInteger.valueOf(13))
+        val b = FieldElement(BigInteger.valueOf(12), BigInteger.valueOf(13))
+        val c = FieldElement(BigInteger.valueOf(6), BigInteger.valueOf(13))
         assertEquals(c , a + b)
-        assertEquals(c , a.add(b))
     }
 
     @Test
     fun subtract() {
-        val a = FieldElement(7, 13)
-        val b = FieldElement(12, 13)
-        val c = FieldElement(8, 13)
+        val a = FieldElement(BigInteger.valueOf(7), BigInteger.valueOf(13))
+        val b = FieldElement(BigInteger.valueOf(12), BigInteger.valueOf(13))
+        val c = FieldElement(BigInteger.valueOf(8), BigInteger.valueOf(13))
         assertEquals(c , a - b)
-        assertEquals(c , a.subtract(b))
     }
 
     @Test
     fun multiply() {
-        val a = FieldElement(3, 13)
-        val b = FieldElement(12, 13)
-        val c = FieldElement(10, 13)
+        val a = FieldElement(BigInteger.valueOf(3), BigInteger.valueOf(13))
+        val b = FieldElement(BigInteger.valueOf(12), BigInteger.valueOf(13))
+        val c = FieldElement(BigInteger.valueOf(10), BigInteger.valueOf(13))
         assertEquals(c , a * b)
-        assertEquals(c , a.multiply(b))
     }
 
     @Test
     fun power() {
-        val a = FieldElement(3, 13)
-        val b = FieldElement(1, 13)
-        assertEquals(b , a.pow(3))
+        val a = FieldElement(BigInteger.valueOf(3), BigInteger.valueOf(13))
+        val b = FieldElement(BigInteger.valueOf(1), BigInteger.valueOf(13))
+        assertEquals(b , a.pow(BigInteger.valueOf(3)))
 
-        val c = FieldElement(7, 13)
-        val d = FieldElement(8, 13)
-        assertEquals(d , c.pow(-3))
+        val c = FieldElement(BigInteger.valueOf(7), BigInteger.valueOf(13))
+        val d = FieldElement(BigInteger.valueOf(8), BigInteger.valueOf(13))
+        assertEquals(d , c.pow(BigInteger.valueOf(-3)))
     }
 
     @Test
     fun perma_little_theorem() {
-        val primes = arrayListOf(7, 11, 17, 31)
+        val primes = arrayListOf(
+                BigInteger.valueOf(7),
+                BigInteger.valueOf(11),
+                BigInteger.valueOf(17),
+                BigInteger.valueOf(31))
         primes.forEach { prime ->
-            val one = FieldElement(1, prime)
-            (1 until prime).forEach {
-                assertEquals(one, FieldElement(it, prime).pow(prime - 1))
+            val one = FieldElement(BigInteger.ONE, prime)
+            (1 until prime.toLong()).map { BigInteger.valueOf(it) }. forEach {
+                assertEquals(one, FieldElement(it, prime).pow(prime - BigInteger.ONE))
             }
         }
     }
 
     @Test
     fun division() {
-        val a = FieldElement(9, 13)
-        val b = FieldElement(3, 13)
-        val c = FieldElement(3, 13)
-        val d = FieldElement(7, 13)
-        val e = FieldElement(5, 13)
+        val a = FieldElement(BigInteger.valueOf(9), BigInteger.valueOf(13))
+        val b = FieldElement(BigInteger.valueOf(3), BigInteger.valueOf(13))
+        val c = FieldElement(BigInteger.valueOf(3), BigInteger.valueOf(13))
+        val d = FieldElement(BigInteger.valueOf(7), BigInteger.valueOf(13))
+        val e = FieldElement(BigInteger.valueOf(5), BigInteger.valueOf(13))
         assertEquals(c , a / b)
         assertEquals(e , a / d)
     }
