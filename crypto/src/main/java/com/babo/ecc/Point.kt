c79b3904fcffdd75e686aa6631cc90e6118e12c5
babo <seirion@mensakorea.org>
2019-12-23 19:39:52 +0900

    to BigInteger

--- crypto/src/main/java/com/babo/ecc/Point.kt
+++ crypto/src/main/java/com/babo/ecc/Point.kt
@@ -1,27 +1,31 @@
 package com.babo.ecc
 
-data class Point(var x: Int?, var y: Int?, val a: Int, val b: Int) {
+import java.math.BigInteger
+
+data class Point(var x: BigInteger?, var y: BigInteger?, val a: Int, val b: Int) {
     init {
         require((x == null && y == null) || (x != null && y != null))
         if (x != null && y != null) {
+            val bigA = BigInteger.valueOf(a.toLong())
+            val bigB = BigInteger.valueOf(b.toLong())
             // y^2 = x^3 + ax + b
-            require(y!! * y!! == x!! * x!! * x!! + a * x!! + b) {
+            require(y!!.pow(2) == x!!.pow(3) + bigA * x!! + bigB) {
                 "Point($x, $y) is not on the curve"
             }
         }
     }
 
-    fun add(other: Point) = this + other
+    fun infinity(): Boolean = (x == null) // point of inifinity
 
     operator fun plus(other: Point): Point {
         require(a == other.a && b == other.b) {
-            "Points $this $other are not on the same curve"
+            "Points $this and $other are not on the same curve"
         }
         return when {
             x == null -> other.copy()
             other.x == null -> this.copy()
-            y == -1 * other.y!! -> Point(null, null, a, b)
-            else -> this.copy()
+            y == other.y!!.negate() -> Point(null, null, a, b)
+            else -> this.copy() // not implemented
         }
     }
 }
--- crypto/src/test/java/com/babo/ecc/PointTest.kt
+++ crypto/src/test/java/com/babo/ecc/PointTest.kt
@@ -5,31 +5,36 @@ import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertDoesNotThrow
 import org.junit.jupiter.api.assertThrows
 import java.lang.IllegalArgumentException
+import java.math.BigInteger
 
 class PointTest {
     @Test
     fun testPointOnTheCurve() {
         assertDoesNotThrow {
-            Point(-1, -1, 5, 7)
+            Point(Big_M1, Big_M1, 5, 7)
         }
         assertThrows<IllegalArgumentException> {
-            Point(-1, -2, 5, 7)
+            Point(Big_M1, Big_M2, 5, 7)
         }
     }
 
     @Test
     fun additionOfInf() {
         val p1 = Point(null, null, 5, 7)
-        val p2 = Point(-1, -1, 5, 7)
+        val p2 = Point(Big_M1, Big_M1, 5, 7)
         assertEquals(p2, p1 + p2)
-        assertEquals(p2, p1.add(p2))
     }
 
     @Test
     fun additionalInverse() {
-        val p1 = Point(-1, 1, 5, 7)
-        val p2 = Point(-1, -1, 5, 7)
+        val p1 = Point(Big_M1, Big1, 5, 7)
+        val p2 = Point(Big_M1, Big_M1, 5, 7)
         assertEquals(Point(null, null, 5, 7), p1 + p2)
-        assertEquals(Point(null, null, 5, 7), p1.add(p2))
+    }
+
+    companion object {
+        private val Big_M1 = BigInteger.valueOf(-1L)
+        private val Big_M2 = BigInteger.valueOf(-2L)
+        private val Big1 = BigInteger.ONE
     }
 }
